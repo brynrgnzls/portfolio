8cf6b8f90e045c0e86908590408c4bb203e9c01a
Bryan Gonzales <brynrgnzls@gmail.com>
2024-07-09 11:14:15 +0800

    [UPDATE] Add Root Element to Router

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+
+// Pages
+import Root from "./pages/Root.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1 className="bg-red-400">Hello World</h1>,
+    element: <Root />,
+  },
+  {
   },
 ]);
 
