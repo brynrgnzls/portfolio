94e4b2126ad9ee2df4e7552e19d7ad7b7f2321da
Bryan Gonzales <brynrgnzls@gmail.com>
2024-07-11 23:15:05 +0800

    [UPDATE] Added particle components

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -5,12 +5,13 @@ import MouseTexttInput from "../components/MouseTextInput";
 import Nav from "../components/Navbar";
 import Skills from "../components/Skills";
 import Timeline from "../components/Timeline";
+import Particles from "../components/Particles";
 
 export default function Root() {
   return (
-    <div className="max-sm:pb-16">
+    <div className="max-w-[100vw] overflow-x-hidden max-sm:pb-16">
       <MouseTexttInput />
-      {/* <Particles /> */}
+      <Particles />
       <Nav />
       <Cover />
       <AboutMe />
