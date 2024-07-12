23e6d520eaf1d7614c30de91d9164592da715b12
Bryan Gonzales <brynrgnzls@gmail.com>
2024-07-12 23:40:40 +0800

    [ADD] base server structure
     - mongodb db instance decorated to request instance
     - http2 and https on local server
     - dotenv's for production and deplyment

---
+++ server/index.ts
@@ -0,0 +1,37 @@
+import fastify from "fastify";
+import { fastifyConfig } from "./config";
+import requestDecorators from "./hooks/requestDecorators";
+
+const app = fastify(fastifyConfig);
+
+// ====== CORE PLUGINS ====== //
+
+// ======= DECORATORS =======//
+app.decorateRequest("db", null);
+
+// ======= HOOKS ======= //
+app.addHook("preHandler", requestDecorators);
+app.addHook("onError", async (request, reply, error) => {
+  console.error(error);
+});
+
+// ======= ENDPOINTS ======= //
+
+app.get("/test", async (request, reply) => {
+  request.db.collection("test").insertOne({ test: "test" });
+  return "Hello World!";
+});
+
+app.listen(
+  {
+    port: process.env.SERVER_PORT as unknown as number,
+    host: process.env.SERVER_HOST as string,
+  },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  }
+);
