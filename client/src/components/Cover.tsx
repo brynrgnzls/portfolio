e4c6858cc83ba7aef78b3152d79dce85285453f2
Bryan Gonzales <brynrgnzls@gmail.com>
2024-07-30 16:11:05 +0800

    .\src\components\Footer.tsx

--- client/src/components/Cover.tsx
+++ client/src/components/Cover.tsx
@@ -22,7 +22,7 @@ function Cover() {
 
 function Avatar() {
   return (
-    <div className="glow-accent relative m-5 aspect-square w-56 rounded-full before:rounded-full after:absolute after:h-full after:w-full after:rounded-full after:bg-accent after:blur-lg after:content-[''] sm:mb-8 sm:w-72 md:w-1/2 md:before:rounded-lg md:after:rounded-lg md:max-xl:min-w-80 xl:max-w-[550px]">
+    <div className="glow-accent max relative m-5 aspect-square w-56 rounded-full before:rounded-full after:absolute after:h-full after:w-full after:rounded-full after:bg-accent after:blur-lg after:content-[''] sm:mb-8 sm:w-72 md:w-1/2 md:before:rounded-lg md:after:rounded-lg md:max-lg:w-72 lg:max-xl:min-w-72 xl:max-w-[550px]">
       <img
         src={pfp}
         alt="Image of Bryan Gonzales"
@@ -34,20 +34,35 @@ function Avatar() {
 
 function Header() {
   return (
-    <p className="lg:leading mb-3 w-full text-4xl font-semibold leading-normal tracking-wider sm:mb-5 sm:text-5xl 2xl:text-6xl">
-      Hello,{" "}
-      <span className="bg-accent bg-clip-text text-transparent">
-        I'm Bryan.
-      </span>{" "}
-      <br />
-      Full-Stack Developer.
-    </p>
+    <div className="lg:leading 2xl mb-3 inline-block text-4xl font-semibold leading-normal tracking-wider sm:mb-5 sm:text-5xl lg:text-6xl 2xl:text-7xl">
+      <p className="text-animation-name z-50 bg-acccentText bg-clip-text font-mono text-transparent lg:mb-2">
+        Hello I'm Bryan.
+      </p>
+
+      <div className="justify-startfont-mono relative flex items-center">
+        <div className="2xl:6xl relative inline-block font-mono sm:text-4xl lg:text-5xl">
+          <p className="text-animation-frontend overflow-hidden opacity-0">
+            Front-End
+          </p>
+        </div>
+        <div className="2xl:6xl absolute inline-block font-mono sm:text-4xl lg:text-5xl">
+          <p className="text-animation-backend overflow-hidden opacity-0">
+            Back-End
+          </p>
+        </div>
+        <div className="2xl:6xl absolute inline-block font-mono text-2xl sm:text-4xl lg:text-5xl">
+          <p className="text-animation-fullstack overflow-hidden opacity-0">
+            Full-Stack Developer
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
 function Context() {
   return (
-    <p className="mb-5 w-full text-sm text-gray-500 sm:mb-8 sm:text-base md:text-lg 2xl:text-xl">
+    <p className="text-animation-context mb-5 w-full text-sm text-gray-400 opacity-0 sm:mb-8 sm:text-base md:text-lg 2xl:text-xl">
       Aspiring Web Developer Based in the Phillipines. Enthusiastic both in
       front-end and back-end developement. Passionate for building fast and
       optimized websites using the latest technologies.
@@ -57,7 +72,7 @@ function Context() {
 
 function ButtonContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex w-full justify-center gap-4 md:justify-start">
+    <div className="text-animation-context flex w-full justify-center gap-4 opacity-0 md:justify-start">
       {children}
     </div>
   );
@@ -92,11 +107,7 @@ function ViewProjectsButton() {
 }
 
 function ContextLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="max-md:max-w-[550px] max-sm:max-w-sm md:w-full" >
-      {children}
-    </div>
-  );
+  return <div className="w-full max-sm:max-w-[500px]">{children}</div>;
 }
 
 export default memo(Cover);
