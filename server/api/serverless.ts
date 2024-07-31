7411cf86b409158c3535fbb81c1cd7151514e4c4
Bryan Gonzales <brynrgnzls@gmail.com>
2024-07-31 11:52:33 +0800

    [UPDATE] Added ping route

--- server/api/serverless.ts
+++ server/api/serverless.ts
@@ -1,15 +1,20 @@
-import Fastify, {
-  FastifyInstance,
-  FastifyRegister,
-  FastifyRequest,
-} from "fastify";
+import Fastify, { FastifyRegister, FastifyRequest } from "fastify";
 import routes from "../src/app.js";
+import { MyFastifyInstance } from "src/@types/my-fastsify.js";
 
-const app: FastifyInstance = Fastify({
+const app: MyFastifyInstance = Fastify({
   logger: false,
 });
 
-app.register(routes, { prefix: "/" });
+app.register(routes, { prefix: "/ping" });
+
+app.get("/", async (req, res) => {
+  app.pusher.trigger("test-channel", "test-event", { ping: "pong" });
+  const dbAck = (await app.db.collection("test").insertOne({ hello: "world" }))
+    .acknowledged;
+
+  return res.send({ hello: "world", dbAck });
+});
 
 // ======= ENDPOINTS ======= //
 
