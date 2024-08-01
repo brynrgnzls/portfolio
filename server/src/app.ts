f18b8ed941cb10e2251d6112fe72267e0b1bf8d3
Bryan Gonzales <brynrgnzls@gmail.com>
2024-08-01 13:09:00 +0800

    [CHORE] added messenger and pusher routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import { FastifyServerOptions } from "fastify";
 import { fastifyCorsConfig } from "./configs/index.js";
 import type { MyFastifyInstance } from "./@types/my-fastsify.js";
+import { pusher } from "./lib/index.js";
+import { MongoDbConnector } from "./models/index.js";
 
 export default async function Routes(
   instance: MyFastifyInstance,
@@ -9,16 +11,16 @@ export default async function Routes(
 ) {
   instance.register(import("@fastify/cors"), fastifyCorsConfig);
 
-  // instance.decorate("db", await MongoDbConnector.getDbInstance());
-  // instance.decorate("pusher", pusher);
+  instance.decorate("db", await MongoDbConnector.getDbInstance());
+  instance.decorate("pusher", pusher);
 
-  // instance.addHook("onError", (req, res, error, done) => {
-  //   console.error(error);
-  //   done();
-  // });
+  instance.addHook("onError", (req, res, error, done) => {
+    console.error(error);
+    done();
+  });
 
-  // instance.register(import("./routes/messageRoute.js"), { prefix: "/message" });
-  // instance.register(import("./routes/pusherRoute.js"), { prefix: "/pusher" });
+  instance.register(import("./routes/messageRoute.js"), { prefix: "/message" });
+  instance.register(import("./routes/pusherRoute.js"), { prefix: "/pusher" });
 
   instance.get("/test", async (req, res) => {
     return res.send({ "Hello World": "Hello Test" });
