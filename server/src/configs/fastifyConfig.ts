b4aa28e4c25fb530db1b2c85d6d8f89380317e8f
Bryan Gonzales <brynrgnzls@gmail.com>
2024-08-01 12:29:26 +0800

    [CHORE] Remove certs and keys referrence

--- server/src/configs/fastifyConfig.ts
+++ server/src/configs/fastifyConfig.ts
@@ -1,14 +1,3 @@
-import { readFileSync } from "node:fs";
-import { resolve } from "node:path";
-
-const fastifyConfig = {
-  http2: true,
-  http2SessionTimeout: 30000,
-  https: {
-    allowHTTP1: true,
-    key: readFileSync(resolve(`${import.meta.dirname}`, "../../private/cert.key")),
-    cert: readFileSync(resolve(`${import.meta.dirname}`, "../../private/cert.crt")),
-  },
-};
+const fastifyConfig = {};
 
 export default fastifyConfig;
